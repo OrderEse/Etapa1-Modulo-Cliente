b60c05bd1eff12326ebcafcb3a202c7704a9ef84
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-03-09 14:22:20 -0600

    Se agrega contenido a la pagina de ver menu (no responsive)

--- Frontend/moduloCliente/src/app/Componentes/paginas/VerMenu/ver-menu/ver-menu.component.ts
+++ Frontend/moduloCliente/src/app/Componentes/paginas/VerMenu/ver-menu/ver-menu.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderSecundarioComponent } from '../../../reutilizables/header-secundario/header-secundario/header-secundario.component';
+import { FooterComponent } from '../../../reutilizables/footer/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ver-menu',
-  imports: [HeaderSecundarioComponent],
+  standalone: true,
+  imports: [HeaderSecundarioComponent, FooterComponent, CommonModule],
   templateUrl: './ver-menu.component.html',
-  styleUrl: './ver-menu.component.css'
+  styleUrl: './ver-menu.component.css',
 })
 export class VerMenuComponent {
+  mostrarImagenGrande: boolean = false;
 
+  abrirImagen() {
+    this.mostrarImagenGrande = true;
+  }
+
+  cerrarImagen() {
+    this.mostrarImagenGrande = false;
+  }
 }
