00d08933850cbcbcdd50ed03955cbb1b44106519
Josue19-08 <josuemarin2009@hotmail.com>
2025-03-24 12:07:45 -0600

    Se crea la página llamar mesero

---
+++ Frontend/moduloCliente/src/app/Componentes/paginas/LlamarMesero/llamar-mesero/llamar-mesero.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { HeaderSecundarioComponent } from '../../../reutilizables/header-secundario/header-secundario/header-secundario.component';
+import { FooterComponent } from '../../../reutilizables/footer/footer/footer.component';
+
+@Component({
+  selector: 'llamar-mesero',
+  standalone: true,
+  imports: [CommonModule, HeaderSecundarioComponent, FooterComponent],
+  templateUrl: './llamar-mesero.component.html',
+  styleUrl: './llamar-mesero.component.css',
+})
+export class LlamarMeseroComponent {
+  cerrar = false;
+
+  constructor(private router: Router) {}
+
+  mostrarConfirmacion() {
+    this.cerrar = true;
+  }
+
+  volverInicio() {
+    this.router.navigate(['/inicio']);
+  }
+}
