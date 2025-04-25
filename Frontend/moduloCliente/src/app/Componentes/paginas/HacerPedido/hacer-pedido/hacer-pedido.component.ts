f7ac373705db3e7103fb0a0156f6bb3c3743c5da
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-04-25 15:21:24 -0600

    Se agrega funcionalidad de agregar linea y ver menu en hacer pedido

--- Frontend/moduloCliente/src/app/Componentes/paginas/HacerPedido/hacer-pedido/hacer-pedido.component.ts
+++ Frontend/moduloCliente/src/app/Componentes/paginas/HacerPedido/hacer-pedido/hacer-pedido.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { HeaderSecundarioComponent } from '../../../reutilizables/header-secundario/header-secundario/header-secundario.component';
 import { FooterComponent } from '../../../reutilizables/footer/footer/footer.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'hacer-pedido',
-  imports: [HeaderSecundarioComponent, FooterComponent],
+  imports: [HeaderSecundarioComponent, FooterComponent, CommonModule, FormsModule],
   templateUrl: './hacer-pedido.component.html',
   styleUrl: './hacer-pedido.component.css'
 })
 export class HacerPedidoComponent {
+
+  public mostrarImagenGrande: boolean = false;
+
+  public pedidos: any[] = [
+    {nombre: '', cantidad: 1},
+    {nombre: '', cantidad: 1}
+  ];
+
+
+  public agregarLinea() : void {
+    this.pedidos.push({nombre: '', cantidad: 1});
+  };
+
+
+  public abrirModal() : void {
+    this.mostrarImagenGrande = true;
+  };
+
+  public cerrarModal() : void {
+    this.mostrarImagenGrande = false;
+  };
 
 }
