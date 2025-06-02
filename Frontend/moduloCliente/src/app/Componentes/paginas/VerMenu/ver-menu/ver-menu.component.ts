76a89e98f3b58079ba46e78c473b0a9f862a626b
Daniel Barrantes <danielbarrantesq@gmail.com>
2025-06-01 22:10:33 -0600

    Se agrega routerlink a botones de página de ver menú

--- Frontend/moduloCliente/src/app/Componentes/paginas/VerMenu/ver-menu/ver-menu.component.ts
+++ Frontend/moduloCliente/src/app/Componentes/paginas/VerMenu/ver-menu/ver-menu.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderSecundarioComponent } from '../../../reutilizables/header-secundario/header-secundario/header-secundario.component';
 import { FooterComponent } from '../../../reutilizables/footer/footer/footer.component';
 import { CommonModule } from '@angular/common';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'ver-menu',
   standalone: true,
-  imports: [HeaderSecundarioComponent, FooterComponent, CommonModule],
+  imports: [HeaderSecundarioComponent, FooterComponent, CommonModule, RouterLink],
   templateUrl: './ver-menu.component.html',
   styleUrl: './ver-menu.component.css',
 })
